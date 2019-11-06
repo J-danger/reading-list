cd30232e229d3b458b9ed916d08a34fd91b370c7
Stephen Howe <Stevehowe10@gmail.com>
2019-11-06 14:19:20 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+     <Router>
+      <>
+          <Nav />
+          <Switch>
+            <Route exact path="/" render={props => <Books {...props} day="Wednesday" />} />
+            <Route exact path="/books" component={Books} />
+            <Route exact path="/books/:id" component={Detail} />
+            <Route component={NoMatch}/>
+          </Switch>
+      </>
+
+     </Router>
+    </div>
+  );
+}
+
+export default App;
